c151278bd2a86a293db94a199a7fa66c233d2af1
Cody Casselman <cody.casselman@ucalgary.ca>
2024-11-27 16:12:59 -0700

    Update ReceiptEmail.java

--- backend/ReceiptEmail.java
+++ backend/ReceiptEmail.java
@@ -1,5 +1,8 @@
 package backend;
 
 public class ReceiptEmail implements MailSender {
-    
+    @Override
+    public void sendEmail(String recipient, String movieName){
+        // TODO Auto-generated method stub
+    };
 }
